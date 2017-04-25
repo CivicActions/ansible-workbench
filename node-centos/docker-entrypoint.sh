28041bf7aafd9e52bb6722a0199eb935f76b3cf1
David Numan <david.numan@civicactions.com>
2017-04-25 09:45:33 -0400

    Adding centos ansible node

---
+++ node-centos/docker-entrypoint.sh
@@ -0,0 +1,32 @@
+#!/usr/bin/env bash
+set -e
+
+# Check KEYS variable
+if [ ! "${AUTHORIZED_KEYS}" ]; then
+  echo "You must provide a pub ssh key in the AUTHORIZED_KEYS variable"
+fi
+
+echo -e "Adding the following key:\n${AUTHORIZED_KEYS}"
+
+# Add key to root user
+mkdir -p /root/.ssh/
+mkdir -p /var/run/sshd
+echo "${AUTHORIZED_KEYS}" > /root/.ssh/authorized_keys
+chmod 600 /root/.ssh/authorized_keys
+
+# Create new user
+USERNAME=admin
+useradd -ms /bin/bash ${USERNAME}
+id ${USERNAME}
+
+# Add key to new user
+mkdir -p /home/${USERNAME}/.ssh/
+echo "${AUTHORIZED_KEYS}" > /home/${USERNAME}/.ssh/authorized_keys
+chmod 600 /home/${USERNAME}/.ssh/authorized_keys
+chown ${USERNAME}: /home/${USERNAME}/.ssh/authorized_keys
+
+echo "${USERNAME} ALL=(ALL)	NOPASSWD: ALL" >> /etc/sudoers
+
+/usr/bin/ssh-keygen -A
+
+exec "$@"
